cc693e61b509e283692676dae4b12d97b647f526
Cloud <kt286@qq.com>
2022-04-17 15:55:44 +0800

    fix: input current time use "ouj"

--- tables/flypy.lua
+++ tables/flypy.lua
@@ -1,4 +1,13 @@
 local fcitx = require("fcitx")
+local _CHINESE_DIGITS = {
+    [0] = "日",
+    [1] = "一",
+    [2] = "二",
+    [3] = "三",
+    [4] = "四",
+    [5] = "五",
+    [6] = "六"
+}
 
 function date_translator(input)
     if fcitx.currentInputMethod() ~= "flypy" then
@@ -13,8 +22,16 @@ function date_translator(input)
         return os.date("%Y-%m-%d")
     end
 
+    if input == "(当前时间周)" then
+        return os.date("%H:%M 周") .. _CHINESE_DIGITS[tonumber(os.date("%w"))]
+    end
+
+    if input == "(当前时间中文)" then
+        return os.date("%H点%M分")
+    end
+
     if input == "(当前时间)" then
-        return os.date("%H:%M")
+        return os.date("%H:%M:%S")
     end
 end
 
